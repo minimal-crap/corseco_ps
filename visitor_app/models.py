e80592278930fb93ad376bbe573133d7a9d8e6d3
sudhanshu singh chauhan <minimalcrap@gmail.com>
2016-12-23 18:28:28 +0530

    modify models.py to add Visitor model

--- visitor_app/models.py
+++ visitor_app/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Visitor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    whom_to_meet = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="photos", blank=True)
