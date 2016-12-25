52b92c687e0f40b007445be7dd336f8047940a78
sudhanshu singh chauhan <minimalcrap@gmail.com>
2016-12-25 18:24:22 +0530

    modify tests.py to write tests for visitor api

--- visitor_app/tests.py
+++ visitor_app/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+
 
 # Create your tests here.
+class VisitorAPITests(APITestCase):
+    def test_visitor_get_api(self):
+        response = self.client.get('/api/peopleinfo/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_visitor_post_api(self):
+        local_file_absolute_url = '/home/sud/Pictures/blur.png'
+        image_file = open(local_file_absolute_url, 'rb')
+        data = {
+            "name": "sudhanshu",
+            "email": "mailbox.sud@gmail.com",
+            "phone": "9930289385",
+            "whom_to_meet": "aniket",
+            "company_name": "corseco",
+            "photo": image_file
+        }
+        response = self.client.post('/api/peopleinfo/',
+                                    data,
+                                    format='multipart')
+        self.assertEqual(response.status_code, 201)
--- visitor_app/views.py
+++ visitor_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from django.views.decorators.csrf import csrf_exempt
+from visitor_app.models import Visitor
+from visitor_app.serializers import VisitorSerializer
 # Create your views here.
+
+
+class Visitor(generics.GenericAPIView,
+              generics.mixins.CreateModelMixin,
+              generics.mixins.ListModelMixin):
+
+    queryset = Visitor.objects.all()
+    serializer_class = VisitorSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    @csrf_exempt
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
