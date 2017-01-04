bb1e0b3e8d23eee50c73ae6be655c72c27424eca
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-04 23:01:47 +0530

    modify manage script to change project name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "connectedh_ps.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "corseco_ps.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
